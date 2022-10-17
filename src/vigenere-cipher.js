d52b8e1a17c47ee0e73fa3d9e81205d8d6985452
Evgenii Kolesnikov <jekow@yandex.ru>
2022-10-17 08:18:26 +0300

    feat: complete vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  constructor (type) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');  
+    }
+
+    while (key.length < message.length) {
+      key += key; 
+    }
+
+    let messageArr = message.toUpperCase().split('').map(v => this.alphabet.indexOf(v));
+    let alphabetArr = messageArr.filter(v => v !== -1);
+    let keyArr = key.toUpperCase().split('').map(v => this.alphabet.indexOf(v));
+    let outArr = alphabetArr.map((v, i) => this.alphabet[(v + keyArr[i])%26])
+    let missedIndexes = 0;
+
+    const outMessage = messageArr.map((v, i) => {
+      if (v !== -1) {
+        return outArr[i - missedIndexes]
+      } else {
+        missedIndexes++;
+        return message[i];
+      }
+    }).join('');
+
+    return this.type === false 
+    ?  [...outMessage].reverse().join('')
+    :  outMessage;
+
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');  
+    }
+
+    while (key.length < message.length) {
+      key += key; 
+    }
+
+    let messageArr = message.toUpperCase().split('').map(v => this.alphabet.indexOf(v));
+    let alphabetArr = messageArr.filter(v => v !== -1);
+    let keyArr = key.toUpperCase().split('').map(v => this.alphabet.indexOf(v));
+    let outArr = alphabetArr.map((v, i) => (v - keyArr[i] > 0)
+      ? this.alphabet[(v - keyArr[i]) % 26]
+      : this.alphabet[(v - keyArr[i] + 26) % 26]);
+    let missedIndexes = 0;
+
+    const outMessage = messageArr.map((v, i) => {
+      if (v !== -1) {
+        return outArr[i - missedIndexes]
+      } else {
+        missedIndexes++;
+        return message[i];
+      }
+    }).join('');
+
+    return this.type === false 
+      ?  [...outMessage].reverse().join('')
+      :  outMessage;
   }
 }
 
